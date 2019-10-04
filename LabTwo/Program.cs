c2e4ef18286ff134e4de805342d5f32c9ebb3167
coutaq <coutaq@gmail.com>
2019-10-04 10:04:58 +0300

    initial commit

---
+++ LabTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskOne.Do();
+        }
+    }
+}
---
+++ LabTwo/TaskOne.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LabTwo
+{
+    public class TaskOne
+    {
+        internal static void Do()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
